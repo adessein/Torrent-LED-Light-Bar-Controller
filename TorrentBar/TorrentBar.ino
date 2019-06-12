50ce5979888a3897365b1d722bf2a11e0dfb11ea
Arnaud Dessein <adessein@protonmail.com>
2019-06-12 21:21:38 +0200

    Changed the HTML code so the checkboxes will triger the form submit when they are clicked, whatever their state. Then I added tags {Cn} that are modified in the ESP8266 program by the actual state of the checkboxes (*checked* if checked and nothing if unchecked. Therefore the ESP8266 need to regenerate an index page that reflects the state of the different toggle switches, and return it after each submit. I have also found a way to inclue the HTML code in the ESP8266 in a way that does not require to excape the doublequotes. The file TorrentBar.html is therefore redundant and is only used for testing the page in a browser.

--- TorrentBar/TorrentBar.ino
+++ TorrentBar/TorrentBar.ino
@@ -24,32 +24,192 @@ IPAddress apIP(192, 168, 1, 1);
 DNSServer dnsServer;
 ESP8266WebServer webServer(80);
 
-const char INDEX_HTML[] =
-"<!DOCTYPE html>\n<html>\n<head>\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\" charset=\"utf-8\" />\n<style>\nbody {\n  font-family: Sans-serif;\n}\n\ninput[class=\"bttn\"],p{\n    font-size:24px;\n}\n\n.switch {\n  position: relative;\n  display: inline-block;\n  width: 60px;\n  height: 34px;\n}\n\n.switch input { \n  opacity: 0;\n  width: 0;\n  height: 0;\n}\n\n.slider {\n  position: absolute;\n  cursor: pointer;\n  top: 0;\n  left: 0;\n  right: 0;\n  bottom: 0;\n  background-color: #ccc;\n  -webkit-transition: .4s;\n  transition: .4s;\n  background-color: #ca2222;\n}\n\n.slider:before {\n  position: absolute;\n  content: \"\";\n  height: 26px;\n  width: 26px;\n  left: 4px;\n  bottom: 4px;\n  background-color: white;\n  -webkit-transition: .4s;\n  transition: .4s;\n}\n\ninput:checked + .slider {\n  background-color: #2ab934;\n}\n\ninput:focus + .slider {\n  box-shadow: 0 0 1px #2196F3;\n}\n\ninput:checked + .slider:before {\n  -webkit-transform: translateX(26px);\n  -ms-transform: translateX(26px);\n  transform: translateX(26px);\n}\n\n.slider.round {\n  border-radius: 34px;\n}\n\n.slider.round:before {\n  border-radius: 50%;\n}\n</style>\n\n<!-- https://stackoverflow.com/a/10420584/6863846 -->\n<script src=\"jquery.min.js\"></script>\n<input type=\"hidden\" name=\"action\" id=\"action\" />\n<script language=\"javascript\" type=\"text/javascript\">\n  \n  $(document).ready(function () {\n  //when a submit button is clicked, put its name into the action hidden field\n    $(\":checkbox\").click(function() { $(this).closest(\"form\").submit();});\n    $(\":submit\").click(function () { $(\"#action\").val(this.name); $(this).closest(\"form\").submit();});\n  });\n</script>\n</head>\n\n<body>\n\n<form>\n\n<h2>Program selection</h2>\n<p><input type=\"submit\" class=\"bttn\" value=\"Mode 1\" name=\"prog\" /></p>\n<p><input type=\"submit\" class=\"bttn\" value=\"Mode 2\" name=\"prog\" /></p>\n<p><input type=\"submit\" class=\"bttn\" value=\"Mode 3\" name=\"prog\" /></p>\n<p><input type=\"submit\" class=\"bttn\" value=\"Cruise mode\" name=\"prog\" /></p>\n<p><input type=\"submit\" class=\"bttn\" value=\"Rear Left Arrow\" name=\"prog\" /></p>\n<p><input type=\"submit\" class=\"bttn\" value=\"Rear Right Arrow\" name=\"prog\" /></p>\n<p><input type=\"submit\" class=\"bttn\" value=\"Take Dowmns\" name=\"prog\" /></p>\n\n<h2>Toggle Switch</h2>\n\n<p>\n<label class=\"switch\">\n  <input type=\"checkbox\" name=\"RightSide\">\n  <span class=\"slider round\"></span>\n</label>\nEnable right-side alley lights\n</p>\n\n<p>\n<label class=\"switch\">\n  <input type=\"checkbox\" name=\"LeftSide\">\n  <span class=\"slider round\"></span>\n</label>\nEnable left-side alley lights\n</p>\n\n<p>\n<label class=\"switch\">\n  <input type=\"checkbox\" name=\"Flashing\">\n  <span class=\"slider round\"></span>\n</label>\nEnable flashing\n</p>\n\n<p>\n<label class=\"switch\">\n  <input type=\"checkbox\" name=\"HighPower\">\n  <span class=\"slider round\"></span>\n</label>\nHigh power\n</p>\n\n<p>\n<label class=\"switch\">\n  <input type=\"checkbox\" name=\"FlashTD\">\n  <span class=\"slider round\"></span>\n</label>\nFlash TDs and alleys\n</p>\n\n<p>\n<label class=\"switch\">\n  <input type=\"checkbox\" name=\"DisableFront\">\n  <span class=\"slider round\"></span>\n</label>\nDisable front lights\n</p>\n\n<p>\n<label class=\"switch\">\n  <input type=\"checkbox\" name=\"DisableR\">\n  <span class=\"slider round\"></span>\n</label>\nDisable read lights\n</p>\n\n</form>\n\n</body>\n</html> \n"
-;
+const char index_page[] = R"=====(
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1" charset="utf-8" />
+<style>
+body {
+  font-family: Sans-serif;
+}
+
+input[class="bttn"],p{
+    font-size:24px;
+}
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input { 
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+  background-color: #ca2222;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2ab934;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+</style>
+
+<!-- https://stackoverflow.com/a/10420584/6863846 -->
+<input type="hidden" name="action" id="action" />
+
+<script language="javascript" type="text/javascript">
+  $(document).ready(function () {
+    //when a submit button is clicked, put its name into the action hidden field
+    $(":submit").click(function () { $("#action").val(this.name); $(this).closest("form").submit();});
+  });
+</script>
+</head>
+
+<body>
+
+<form>
+
+<h2>Program selection</h2>
+<p><input type="submit" class="bttn" value="Mode 1" name="prog" /></p>
+<p><input type="submit" class="bttn" value="Mode 2" name="prog" /></p>
+<p><input type="submit" class="bttn" value="Mode 3" name="prog" /></p>
+<p><input type="submit" class="bttn" value="Cruise mode" name="prog" /></p>
+<p><input type="submit" class="bttn" value="Rear Left Arrow" name="prog" /></p>
+<p><input type="submit" class="bttn" value="Rear Right Arrow" name="prog" /></p>
+<p><input type="submit" class="bttn" value="Take Dowmns" name="prog" /></p>
+
+<h2>Modifiers</h2>
+
+<p>
+<label class="switch">
+  <input type="checkbox" name="RightSide" onClick="this.form.submit()" value='1' {C1}>
+  <span class="slider round"></span>
+</label>
+Enable right-side alley lights
+</p>
+
+<p>
+<label class="switch">
+  <input type="checkbox" name="LeftSide" onClick="this.form.submit()" value='1' {C2}>
+  <span class="slider round"></span>
+</label>
+Enable left-side alley lights
+</p>
+
+<p>
+<label class="switch">
+  <input type="checkbox" name="Flashing" onClick="this.form.submit()" value='1' {C3}>
+  <span class="slider round"></span>
+</label>
+Enable flashing
+</p>
+
+<p>
+<label class="switch">
+  <input type="checkbox" name="HighPower" onClick="this.form.submit()" value='1' {C4}>
+  <span class="slider round"></span>
+</label>
+High power
+</p>
+
+<p>
+<label class="switch">
+  <input type="checkbox" name="FlashTD" onClick="this.form.submit()" value='1' {C5}>
+  <span class="slider round"></span>
+</label>
+Flash TDs and alleys
+</p>
+
+<p>
+<label class="switch">
+  <input type="checkbox" name="DisableFront" onClick="this.form.submit()" value='1' {C6}>
+  <span class="slider round"></span>
+</label>
+Disable front lights
+</p>
+
+<p>
+<label class="switch">
+  <input type="checkbox" name="DisableR" onClick="this.form.submit()" value='1' {C7}>
+  <span class="slider round"></span>
+</label>
+Disable read lights
+</p>
+
+</form>
+
+</body>
+</html> 
+
+)=====";
 
 void setup()
 {
-	WiFi.mode(WIFI_AP);
-	WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-	WiFi.softAP(ssid);
+        WiFi.mode(WIFI_AP);
+        WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+        WiFi.softAP(ssid);
 
-	Serial.begin(115200);
-	
-	// if DNSServer is started with "*" for domain name, it will reply with
-	// provided IP to all DNS request
-	dnsServer.start(DNS_PORT, "*", apIP);
+        Serial.begin(115200);
+        
+        // if DNSServer is started with "*" for domain name, it will reply with
+        // provided IP to all DNS request
+        dnsServer.start(DNS_PORT, "*", apIP);
 
-	webServer.on("/", handleRoot);
-	webServer.onNotFound(handleRoot);
+        webServer.on("/", handleRoot);
+        webServer.onNotFound(handleRoot);
   
-	webServer.begin();
+        webServer.begin();
 }
 
 void loop()
 {
-	dnsServer.processNextRequest();
-	webServer.handleClient();
+        dnsServer.processNextRequest();
+        webServer.handleClient();
 }
 
 void handleRoot()
@@ -66,7 +226,15 @@ void handleRoot()
     handleSubmit();
   }
   else {
-    webServer.send(200, "text/html", INDEX_HTML);
+    String index_str(index_page);
+    index_str.replace("{C1}","");
+    index_str.replace("{C2}","checked");
+    index_str.replace("{C3}","0");
+    index_str.replace("{C4}","checked");
+    index_str.replace("{C5}","checked");
+    index_str.replace("{C6}","0");
+    index_str.replace("{C7}","0");
+    webServer.send(200, "text/html", index_str.c_str());
   }
 }
 
@@ -74,28 +242,28 @@ void handleSubmit()
 {
   Serial.println("handleSubmit");
 
-	if (webServer.hasArg("prog"))
-	{
+  if (webServer.hasArg("prog"))
+  {
     Serial.println("prog");
-		Serial.println(webServer.arg("prog"));
-	}
+    Serial.println(webServer.arg("prog"));
+    }
 
-	if (webServer.hasArg("RightSide"))
-	{
+  if (webServer.hasArg("RightSide"))
+  {
     Serial.print("RightSide= ");
-		Serial.println(webServer.arg("RightSide"));
-	}
-	
-	/*
-	if (LEDvalue == "1")
-	{
-		writeLED(true);
-		server.send(200, "text/html", INDEX_HTML);
-	}
-	else if (LEDvalue == "0")
-	{
-		writeLED(false);
-		server.send(200, "text/html", INDEX_HTML);
-	}
-	*/
+    Serial.println(webServer.arg("RightSide"));
+  }
+  
+  /*
+  if (LEDvalue == "1")
+  {
+    writeLED(true);
+    server.send(200, "text/html", INDEX_HTML);
+  }
+  else if (LEDvalue == "0")
+  {
+    writeLED(false);
+    server.send(200, "text/html", INDEX_HTML);
+  }
+  */
 }
